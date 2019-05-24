04e6b981a8bddf8c32b8d2ef3db12bf0dc55b276
Alex <alexander.adamlaurence@gmail.com>
2019-05-24 21:30:14 +0100

    app-details

---
+++ DoReMi-app/saas/app/views/main.py
@@ -0,0 +1,39 @@
+from flask import render_template, jsonify, request
+from app import app
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html', title='Map')
+
+
+@app.route('/map/refresh', methods=['POST'])
+def map_refresh():
+    points = [(random.uniform(48.8434100, 48.8634100),
+               random.uniform(2.3388000, 2.3588000))
+              for _ in range(random.randint(2, 9))]
+    return jsonify({'points': points})
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
